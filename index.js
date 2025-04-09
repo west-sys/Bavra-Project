20171fa68d11caeb3cb31e548e83d1fb9c7fe631
West Hustle <ma6154388@gmail.com>
2025-04-09 19:53:39 +0200

    ss

--- index.js
+++ index.js
@@ -21,4 +21,31 @@ if ("geolocation" in navigator) {
     // Geolocation is not supported by the browser
     console.error("Geolocation is not supported by this browser.");
   }
-  logToServer();
+
+  function logToServer() {
+
+    let location;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+        function (position) {
+        location = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        }    
+        );
+    }
+
+    console.log("this is the location log", location);
+
+    fetch('/api/log', {
+        method: 'POST',
+        headers: {
+             'content-type': 'application/json',
+        },
+        body: JSON.stringify({ location }),
+    })
+    .catch(error => console.error('Error sending log to server:', error));
+}
+
+logToServer();
