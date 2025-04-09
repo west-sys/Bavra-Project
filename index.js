eb6be54b1d2915284827abfc0b5f6e19a1490be2
West Hustle <ma6154388@gmail.com>
2025-04-09 08:41:07 +0200

    s

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-function success(position) {
-    doSomething(position.coords.latitude, position.coords.longitude);
+if ("geolocation" in navigator) {
+    /* geolocation is available */
+  } else {
+    /* geolocation IS NOT available */
   }
-  
-  function error() {
-    alert("Sorry, no position available.");
-  }
-  
-  const options = {
-    enableHighAccuracy: true,
-    maximumAge: 30000,
-    timeout: 27000,
-  };
-  
-  const watchID = navigator.geolocation.watchPosition(success, error, options);
   
