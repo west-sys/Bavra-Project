6ff312137de8d85f2f81688835a0b7f0479ccef5
West Hustle <ma6154388@gmail.com>
2025-04-09 20:55:40 +0200

    c

--- index.js
+++ index.js
@@ -22,30 +22,54 @@ if ("geolocation" in navigator) {
     console.error("Geolocation is not supported by this browser.");
   }
 
-  function logToServer() {
+//   function logToServer() {
 
-    let location;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-        function (position) {
-        location = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-        }    
-        );
-    }
+//     let location;
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(
+//         function (position) {
+//         location = {
+//             lat: position.coords.latitude,
+//             lng: position.coords.longitude
+//         };
+//         }    
+//         );
+//     }
 
-    console.log("this is the location log", location);
+//     console.log("this is the location log", location);
 
-    fetch('/api/log', {
-        method: 'POST',
-        headers: {
-             'content-type': 'application/json',
-        },
-        body: JSON.stringify({ location }),
-    })
-    .catch(error => console.error('Error sending log to server:', error));
+//     fetch('/api/log', {
+//         method: 'POST',
+//         headers: {
+//              'content-type': 'application/json',
+//         },
+//         body: JSON.stringify({ location }),
+//     })
+//     .catch(error => console.error('Error sending log to server:', error));
+// }
+
+// logToServer();
+
+
+fetch('/api/location', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(coordinates),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+(error) => {
+  console.error('Error getting location:', error.message);
 }
 
-logToServer();
+ else {
+console.error('Geolocation is not supported by this browser.');
+}
